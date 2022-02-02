817e4bc1e6cd6cf3b99834bca250016582935298
Emily Martinek <emily.martinek12@gmail.com>
2022-02-02 16:30:05 +0100

    bookmark model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_144028) do
+ActiveRecord::Schema.define(version: 2022_02_02_152945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
